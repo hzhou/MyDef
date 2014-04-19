17f8d02732b404591d7b9a9bc38d4efb8270f6c5
Hui Zhou <hzhou@hzsolutions.net>
2014-04-19 07:50:55 -0500

    modified:   bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,12 @@ cd cmp
 perl Makefile.PL INSTALL_BASE=$HOME #pmake
 make install
 cd ..
+perl install_def.pl
 mydef_make.pl # MyDef, perl
 touch perlmake.def mydef.def
 make
 cd MyDef
 perl Makefile.PL INSTALL_BASE=$HOME #pmake
-ln -s ../deflib .
 cd ..
 make install
 
