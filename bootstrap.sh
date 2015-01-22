67e2b3424aa4f1ee21b0520bc18e1be99f5042f7
Hui Zhou <hzhou@aqua.dragontiger.org>
2015-01-22 08:33:37 -0600

    modified:   bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,12 @@ perl Makefile.PL INSTALL_BASE=$HOME #pmake
 make install
 cd ..
 perl install_def.pl
-mydef_make.pl # MyDef, perl
+mydef_make # MyDef, perl
 touch perlmake.def mydef.def
 make
 cd MyDef
 perl Makefile.PL INSTALL_BASE=$HOME #pmake
 cd ..
-mydef_make.pl
+mydef_make
 make install
 
