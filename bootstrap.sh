d6a5412a1f460a22675420b6cb8e677ff51a0c64
Hui Zhou <hzhou@hzsolutions.net>
2016-06-07 23:06:27 -0500

    modified:   bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,11 @@ make install
 cd ..
 
 # install deflib 
-mydef_install deflib .
+$HOME/bin/mydef_install deflib .
 
 # compile from source
 #    note: only those source files that are newer
-mydef_make # MyDef, perl
+$HOME/bin/mydef_make # MyDef, perl
 touch perlmake.def mydef.def
 make
 
@@ -19,6 +19,11 @@ make
 cd MyDef
 perl Makefile.PL INSTALL_BASE=$HOME #pmake
 cd ..
-mydef_make
+$HOME/bin/mydef_make
 make install
 
+echo "# ----------------------------------------------------"
+echo "By Default, MyDef is intalled in $HOME/bin and $HOME/lib"
+echo "    to use MyDef, you need add $HOME/bin to your PATH"
+echo "    and set PERL5LIB=$HOME/lib/perl5 and MYDEFLIB=$HOME/lib/MyDef"
+echo "    It is recommended to set them in your ~/.bashrc"
