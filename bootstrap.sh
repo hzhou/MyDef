c55af8c3ad76dc96747e985c746924f141eb449f
Hui Zhou <hzhou@hzsolutions.net>
2017-03-25 23:35:00 -0500

    modified:   bootstrap.sh 	modified:   mydef_install.def

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,30 @@
 C='\033[0;32m'
 NC='\033[0m'
-printf "\n${C}#---- Install from bootstrap [Perl package] ----${NC}\n"
-cd bootstrap
-perl Makefile.PL INSTALL_BASE=$HOME #pmake
-make install
-cd ..
+
 if [ -z "$MYDEFLIB" ]; then
     printf "\n${C}#---- New install, set PERL5LIB, MYDEFLIB, and PATH ----${NC}\n"
     NEWINSTALL=1
+    install -m555 bootstrap/script/mydef_install $HOME/bin/
+
     PATH=$HOME/bin:/bin:/usr/bin:/usr/local/bin
     export PERL5LIB=$HOME/lib/perl5
     export MYDEFLIB=$HOME/lib/MyDef
 fi
-printf "\n${C}#---- Install MyDef library ----${NC}\n"
-mydef_install deflib $HOME/lib/MyDef
+
+printf "\n${C}#---- Install from bootstrap [Perl package] ----${NC}\n"
+mydef_install bootstrap/script $HOME/bin -
+mydef_install bootstrap/lib    $HOME/lib/perl5 pm
+mydef_install deflib           $HOME/lib/MyDef def
+
 printf "\n${C}#---- Compile from fresh MyDef source ----${NC}\n"
 mydef_make
-touch perlmake.def mydef.def
+touch mydef.def
 make
+
 printf "\n${C}#---- Install updated MyDef ----${NC}\n"
-cd MyDef
-perl Makefile.PL INSTALL_BASE=$HOME #pmake
-cd ..
-mydef_make
-make install
+mydef_install MyDef/lib    . pm
+mydef_install MyDef/script . -
+
 if [ "$NEWINSTALL" = 1 ]; then
     printf "\n${C}#---- MyDef INSTALLED ----${NC}\n"
     echo "By Default, MyDef is intalled in $HOME/bin and $HOME/lib"
