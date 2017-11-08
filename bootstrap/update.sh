78353f9a8535a50ca49488838ddf46c671d56399
Hui Zhou <hzhou@hzsolutions.net>
2017-11-07 19:58:35 -0600

    modified:   update.sh 	modified:   ../deflib/ext.def

--- bootstrap/update.sh
+++ bootstrap/update.sh
@@ -1,4 +1,7 @@
 cp -rv ../MyDef/lib ../MyDef/script ./
-cp -v $HOME/lib/perl5/MyDef/*.pm lib/MyDef/
+
+for a in compileutil.pm  dumpout.pm  output_general.pm  output_perl.pm  parseutil.pm  regex.pm  utils.pm; do
+    cp -v $HOME/lib/perl5/MyDef/$a lib/MyDef/
+done
 
 cp -v $HOME/lib/MyDef/std_perl.def deflib/
