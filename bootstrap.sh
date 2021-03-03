76f2a3d189d560e077f93103a03b7cd4d9fb4ed1
Hui Zhou <hzhou@hzsolutions.net>
2021-03-02 22:40:38 -0600

    Update bootstrap.sh to use MyDef_boot

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,58 +1,60 @@
 C='\033[0;32m'
 NC='\033[0m'
 
-MY_INSTALL="perl bootstrap/script/mydef_install"
-
 if [ -z "$MYDEFLIB" ]; then
-    printf "\n${C}#---- New install, set PERL5LIB, MYDEFLIB, and PATH ----${NC}\n"
+    printf "\n${C}#---- New install ----${NC}\n"
     NEWINSTALL=1
-    install -d $HOME/bin
-    install -d $HOME/lib
-    install -d $HOME/lib/perl5
-    install -d $HOME/lib/MyDef
-
-    PATH=$HOME/bin:$PATH
-    export PERL5LIB=$HOME/lib/perl5
-    export MYDEFLIB=$HOME/lib/MyDef
+else
+    printf "\n${C}#---- Install Path ----${NC}\n"
+    echo PATH:     $PATH
+    echo PERL5LIB: $PERL5LIB
+    echo MYDEFLIB: $MYDEFLIB
+    save_PATH=$PATH
+    save_PERL5LIB=$PERL5LIB
+    save_MYDEFLIB=$MYDEFLIB
 fi
 
-printf "\n${C}#---- Install from bootstrap ----${NC}\n"
-for a in parseutil compileutil dumpout utils output_perl; do
-    touch bootstrap/lib/MyDef/$a.pm
-done
-touch bootstrap/lib/MyDef.pm
-touch bootstrap/script/mydef_make
-touch bootstrap/script/mydef_page
-$MY_INSTALL bootstrap/script . -
-$MY_INSTALL bootstrap/lib    . pm
-$MY_INSTALL bootstrap/deflib . def
-$MY_INSTALL -f deflib        . def
+BOOT=mydef_boot
+export PATH=$BOOT/bin:$PATH
+export PERL5LIB=$BOOT/lib/perl5
+export MYDEFLIB=$BOOT/lib/MyDef
 
-if [ x$1 = xall ]; then
-    $MY_INSTALL bootstrap/all_lib    . pm
-    $MY_INSTALL bootstrap/all_deflib . def
-fi 
+printf "\n${C}#---- Compile from fresh MyDef source ----${NC}\n"
+perl $BOOT/bin/mydef_make
+touch *.def
+make
 
-# In case some system do not record file stamps higher than 1 sec.
+printf "\n${C}#---- Install updated MyDef ----${NC}\n"
+if [ "$NEWINSTALL" = 1 ]; then
+    bin_dir=$HOME/bin
+    lib_dir=$HOME/lib
 
-if [ x$1 != xskip ]; then # so "sh bootstrap.sh skip" will skip these
-    sleep 1
-    printf "\n${C}#---- Compile from fresh MyDef source ----${NC}\n"
-    perl bootstrap/script/mydef_make
-    touch *.def
-    make
-    printf "\n${C}#---- Install updated MyDef ----${NC}\n"
-    $MY_INSTALL MyDef/lib    . pm
-    $MY_INSTALL MyDef/script . -
+    install -d $bin_dir
+    install -d $lib_dir
+    install -d $lib_dir/perl5
+    install -d $lib_dir/MyDef
+
+    export PATH=$bin_dir:$PATH
+    export PERL5LIB=$lib_dir/perl5
+    export MYDEFLIB=$lib_dir/MyDef
+else    
+    export PATH=$save_PATH
+    export PERL5LIB=$save_PERL5LIB
+    export MYDEFLIB=$save_MYDEFLIB
 fi
+
+MY_INSTALL="perl MyDef/script/mydef_install"
+$MY_INSTALL deflib       . def
+$MY_INSTALL MyDef/lib    . pm
+$MY_INSTALL MyDef/script . -
 
 if [ "$NEWINSTALL" = 1 ]; then
     printf "\n${C}#---- MyDef INSTALLED ----${NC}\n"
-    echo "By Default, MyDef is intalled in $HOME/bin and $HOME/lib"
+    echo "By Default, MyDef is intalled in $bin_dir and $lib_dir"
     echo "    to use MyDef, you need:" 
-    echo "    * add $HOME/bin to your PATH"
-    echo "    * set PERL5LIB=$HOME/lib/perl5"
-    echo "    * set MYDEFLIB=$HOME/lib/MyDef"
+    echo "    * add $bin_dir to your PATH"
+    echo "    * set PERL5LIB=$lib_dir/perl5"
+    echo "    * set MYDEFLIB=$lib_dir/MyDef"
     echo "    * set MYDEFSRC=`pwd`"
     echo "    It is recommended to set them in your ~/.bashrc"
 fi
